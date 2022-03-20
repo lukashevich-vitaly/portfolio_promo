37f656fc570d730081c736f7931ad4949ba4b18e
lukashevich-vitaly <lykashevich.vitalij@gmail.com>
2022-03-21 00:50:37 +0300

    fix: fixed modal

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,15 @@
-const modalButton = document.querySelector('.more');
+const modalButton = document.querySelectorAll('.more');
 const modal = document.querySelector('.modal');
 const overlay = modal.querySelector('.overlay');
 const closeButton = modal.querySelector('.modal__close');
 
 
-modalButton.addEventListener('click', () => {
-    modal.classList.remove('hidden')
+modalButton.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        modal.classList.remove('hidden')
+    })
 });
+
 
 overlay.addEventListener('click', () => {
     modal.classList.add('hidden')
