9dec5dbbd62d8ba09d200f7547b7d3a95163b778
lukashevich-vitaly <lykashevich.vitalij@gmail.com>
2022-03-21 22:09:17 +0300

    feat: create smooth scroll

---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+const links = document.querySelectorAll('.menu-list__link');
+const btn = document.querySelector('.main__button');
+const a = document.querySelector('.main__scroll');
+
+const allLinks = [...links, btn, a];
+
+allLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault()
+        
+        const id = link.getAttribute('href').substring(1)
+        const section = document.getElementById(id)
+
+        if (section) {
+            seamless.scrollIntoView(section, {
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+            });
+        }
+    });
+});
