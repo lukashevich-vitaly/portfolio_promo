9888cb525b98e26dde59ed036992d32404493235
lukashevich-vitaly <lykashevich.vitalij@gmail.com>
2022-03-21 22:27:43 +0300

    fix: fix timer and add days timer

--- js/timer.js
+++ js/timer.js
@@ -7,20 +7,22 @@ const updateClock = () => {
     const dateDeadline = new Date(deadline).getTime();
     const dateRemaining = (dateDeadline - date) / 1000;
     
+    const days = Math.floor(dateRemaining / 60 /60 /24);
     const hours = Math.floor(dateRemaining / 60 /60);
     const minutes = Math.floor((dateRemaining / 60) % 60);
     const seconds = Math.floor(dateRemaining % 60);
     
-    const fHours = hours < 10 ? '0' + hours : hours;
+    const fdays = days < 10 ? '0' + days : days;
+    const fHours = (hours - fdays*24) < 10 ? '0' + (hours - fdays*24) : (hours - fdays*24);
     const fMinutes = minutes < 10 ? '0' + minutes : minutes;
     const fSeconds = seconds < 10 ? '0' + seconds : seconds;
 
-    timerBlock.textContent = `${fHours}:${fMinutes}:${fSeconds}`;
+    timerBlock.textContent = `${fdays}:${fHours}:${fMinutes}:${fSeconds}`;
 
     if (dateRemaining <= 0) {
         clearInterval(interval)
         timerBlock.textContent = '00:00:00';
-    }
+    };
 };
 
 updateClock();
